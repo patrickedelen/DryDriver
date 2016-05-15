8107db08857bf8563a2c33ea7b9dff5256722263
Ivan Berry <ivan.berry@gmail.com>
2016-05-14 20:40:56 -0500

    Added FWS Scrape

--- API/Dry Driver/Dry Driver Console Backend/Program.cs
+++ API/Dry Driver/Dry Driver Console Backend/Program.cs
@@ -9,6 +9,7 @@ using System.Net;
 using System.Runtime.Serialization.Json;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web.Script.Serialization;
 
 namespace Dry_Driver_Console_Backend
 {
@@ -36,8 +37,8 @@ namespace Dry_Driver_Console_Backend
                         if (stringData.Count() > 8)
                         {
 
-                            floodData.Cordinate[0] = decimal.Parse(stringData[1]);
-                            floodData.Cordinate[1] = decimal.Parse(stringData[2]);
+                            floodData.Cordinate[0] = decimal.Parse(stringData[2]);
+                            floodData.Cordinate[1] = decimal.Parse(stringData[1]);
                             floodData.Address = stringData[3].Trim();
                             floodData.CreateDate = DateTime.Parse(stringData[4]);
                             if (stringData[5].Trim() != "NULL")
@@ -66,8 +67,62 @@ namespace Dry_Driver_Console_Backend
             var client = new MongoClient(connectionString);
             var database = client.GetDatabase("drydriver");
             var collection = database.GetCollection<BsonDocument>("events");
-            collection.InsertMany(result);
+            //collection.InsertMany(result);
             //Console.ReadKey();
+
+            ///
+            /// FWS Data Post
+            ///
+            Console.WriteLine("Getting Harris County Bayou Levels");
+            result.Clear();
+            HarrisFWS FWSobj = new HarrisFWS();
+            //you can change this to GetFWSjsonStreamCurrent(7) to get current data
+            Task<string> task1 = FWSobj.GetFWSjsonStreamHistorical("4/18/2016 3:18:15 PM", 1440);
+            
+            JavaScriptSerializer jss = new JavaScriptSerializer();
+            var d = jss.Deserialize<dynamic>(task1.Result);
+            var sites = d["Sites"];
+            foreach (var site in sites)
+            {
+                FlodData floodData = new FlodData();
+                if (site["StreamData"] != null) //sometimes null
+                {
+
+                    decimal currentLevel = site["StreamData"][0]["CurrentLevel"];
+                    decimal TOB = site["StreamData"][0]["ChannelInfo"]["TOB"];
+                    if (currentLevel > TOB)
+                    {
+                        Console.WriteLine(site["Location"] + " IS OVERFLOWING");
+                        floodData.Cordinate[0] = site["Longitude"];
+                        floodData.Cordinate[1] = site["Latitude"]; 
+                        floodData.Address = site["Location"];
+                        floodData.CreateDate = DateTime.Now;
+
+                        //floodData.ClosedDate = DateTime.Parse(stringData[5]);  need to consider this somehow
+                        floodData.SourceType = "FWS";
+                        floodData.MetaData.CaseType = "Flooding";
+                        floodData.MetaData.CaseNumber = site["SiteId"];
+                        
+                        //Console.WriteLine(site["StreamData"][0]["CurrentLevel"]);
+                        //Console.WriteLine(site["StreamData"][0]["ChannelInfo"]["TOB"]);
+                        using (MemoryStream jsonStream = new MemoryStream())
+                        {
+                            DataContractJsonSerializer serializer = new DataContractJsonSerializer(typeof(FlodData));
+                            serializer.WriteObject(jsonStream, floodData);
+                            jsonStream.Position = 0;
+                            StreamReader sr = new StreamReader(jsonStream);
+                            string post = sr.ReadToEnd();
+
+                            result.Add(BsonSerializer.Deserialize<BsonDocument>(post));
+                        }
+
+                    }
+                   
+                }
+
+            }
+            //write to Mongo
+            collection.InsertMany(result);
         }
     }
 }
