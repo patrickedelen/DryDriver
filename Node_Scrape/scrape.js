b25486a5c47bcea7c9c416be6c5b4b86f29c06f1
Patrick Edelen <patrickedelen@gmail.com>
2016-06-09 23:42:56 -0500

    Worked on formatting the returned data

--- Node_Scrape/scrape.js
+++ Node_Scrape/scrape.js
@@ -4,11 +4,21 @@ var cheerio = require('cheerio');
 var url = 'http://houstontx.gov/heatmaps/datafiles/floodingheatmap72all.txt';
 console.log('Working!');
 
+var findAddress = function(body) {
+	var lines = body.split('\n');
+
+	console.log('Line 1: ' + lines[2]);
+
+	var address = lines[2].split('|')[3];
+	console.log('Line 1 Address: ' + address);
+}
+
 request(url, function(err, resp, body){
-	console.log('requesting...' + body);
-  $ = cheerio.load(body);
-  links = $('.sb_tlst h3 a'); //selecting the address
-  $(links).each(function(i, link){
-    console.log($(link).text() + ':\n  ' + $(link).attr('href'));
-  });
+	console.log('requesting...');
+	findAddress(body);
+  // $ = cheerio.load(body);
+  // links = $('.sb_tlst h3 a'); //selecting the address
+  // $(links).each(function(i, link){
+  //   console.log($(link).text() + ':\n  ' + $(link).attr('href'));
+  // });
 });
