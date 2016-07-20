0e109148a4e8dafa9b67f9a0bd2daadff60a4a19
Patrick Edelen <patrickedelen@gmail.com>
2016-07-19 23:51:52 -0500

    Started socket.io integration

--- Application/app/userAlerts.js
+++ Application/app/userAlerts.js
@@ -1,1 +1,12 @@
 //Stores locations of users and alerts users when an alert/incident happens near their location
+var io = require('socket.io')(http);
+
+io.on('connection', function(socket){
+  console.log('user connected');
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
+});
