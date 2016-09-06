d723121fb7312322c73170ceff51d94fd06d9f70
Patrick Edelen <patrickedelen@gmail.com>
2016-09-05 23:43:04 -0500

    Worked on integrating transtar data

--- Scraper/app/transtar.js
+++ Scraper/app/transtar.js
@@ -1,3 +1,8 @@
 var request    = require('request');
 var async      = require('async');
 
+var transtarUrl = 'http://traffic.houstontranstar.org/data/layers/incidents_json.js';
+
+request.get(transtarUrl, function(err, data) {
+    console.log(data);
+});
